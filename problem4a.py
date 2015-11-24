7ef3c3fe5e82a3f4138c32a4dcfb9fe511a4b022
Arun Sambargi <arun.sambargi@gmail.com>
2015-11-23 21:10:44 -0500

    Largest Palindrome

---
+++ problem4a.py
@@ -0,0 +1,8 @@
+# Problem Header :Largest prime factor
+
+"""
+Problem Description:
+The prime factors of 13195 are 5, 7, 13 and 29.
+What is the largest prime factor of the number 600851475143 ?
+"""
+#Fermat's factorization method
